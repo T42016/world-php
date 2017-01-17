55761b22048565c27fb84970fc1b7734c1be946f
Marrezet <martin.dahren@lund.ntig.se>
2017-01-17 14:28:32 +0100

    Added code

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-</body>
-</html>
+<?php
+require __DIR__ . '/vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($loader, array('cache' => __DIR__ .'/cache',));
+echo $twig->render('index.twig');
+	?>
