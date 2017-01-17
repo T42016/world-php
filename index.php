a0a658ba2053dc429d78ee72ec2d40ba4bcf6b22
runeanderberg <rune.anderberg@lund.ntig.se>
2017-01-17 10:01:45 +0100

    created index page

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    require __DIR__.'/vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+    $twig = new Twig_Environment($loader, array('debug' => true));
+
+    echo $twig->render('index.twig')
+?>
