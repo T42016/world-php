c56677b481e96e76d9f4ef82f0dfabac4ff60c7f
runeanderberg <rune.anderberg@lund.ntig.se>
2017-01-17 12:03:49 +0100

    fixes for orderBy links

--- language.php
+++ language.php
@@ -6,7 +6,8 @@
     $db = new PDO('mysql:host=localhost;dbname=world;charset=utf8mb4', 'root', '');
 
     if (isset($_GET["code"])) {
-        $stmt = $db->prepare('SELECT city.Name, country.Name AS CountryName, city.District, city.Population FROM city JOIN country ON city.CountryCode = country.Code WHERE CountryCode = ?');
+        $stmt = $db->prepare('SELECT countrylanguage.Language, country.Name AS Country, country.Code AS Code, countryLanguage.IsOfficial, countrylanguage.Percentage
+            FROM countrylanguage JOIN country ON countrylanguage.CountryCode = country.Code WHERE countrylanguage.CountryCode = ?');
         $stmt->execute([$_GET['code']]);
     }
     else {
