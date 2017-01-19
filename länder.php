30ae9eabc0976e4120a32b4da7b5a31d9e3356d0
Marrezet <martin.dahren@lund.ntig.se>
2017-01-19 08:49:49 +0100

    Added a bit of code

---
+++ länder.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($loader, array('cache' => __DIR__ .'/cache',));
+
+	$pdo = new PDO('mysql:host=localhost;dbname=world;charset=utf8mb4', 'root', '');
+	
+	if(isset($_GET['code']))
+	{
+		$stmt = $pdo->prepare('SELECT * FROM city WHERE CountryCode = ?');
+		$stmt->execute([$_GET['code']]);
+	}
+	else 
+	{
+		$stmt = $pdo->prepare('SELECT * FROM city');
+		$stmt->execute();
+	}
+	/*try {
+		foreach($stmt->fetchAll() as $row) {
+			echo $row['Name'].' '.$row['Population'].'<br/>'; 
+		}
+	} catch(PDOException $ex) {
+		//handle me.
+		echo "ERROR!";
+	}*/
+	
+	echo $twig->render('city.twig', array('cities' => $stmt->fetchAll(),));
+
+?>
